b14fce29fa32ccb9d8ced6c7b8e052ef4a97ce53
488513 <creamripper@gmail.com>
2017-06-14 13:37:52 -0400

    embed invoke-sqlcmd and query into ps

--- folder.ps1
+++ folder.ps1
@@ -5,8 +5,14 @@
 
  )
  write-host $server
+ $query = "update [cs2010].[KIni] SET [IniValue] ='" + $server `
+        + "' WHERE [IniSection] like 'docviewer.Settings' and [IniKeyword] like 'clienturl'"
+
+write-host $query
 
 $Location = "c:\PowerShell\"
-New-Item -Path $Location -Name "Ezines" -ItemType "directory"
+#New-Item -Path $Location -Name "Ezines" -ItemType "directory"
+Invoke-Sqlcmd -ServerInstance Enterprise `
+    -Database CS2010 -UserName sa -Password livelink -Query $query
 
-sqlcmd -S $OldName -i rename.sql -o rename.log
+# sqlcmd -S "Enterprise" -U sa -P livelink -i rename.sql -o rename.log -v url='http://www.yahoo.com'
