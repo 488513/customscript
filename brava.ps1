dfc5c9ad403b7270942021fffc769ee9eef99cb5
488513 <creamripper@gmail.com>
2017-06-14 14:16:05 -0400

    push raw output

---
+++ brava.ps1
@@ -0,0 +1,18 @@
+ # PowerShell's New-Item creates a folder
+
+ param (
+    [string]$server = "http://defaultserver"
+
+ )
+ write-host $server
+ $query = "update [cs2010].[KIni] SET [IniValue] ='" + $server `
+        + "' WHERE [IniSection] like 'docviewer.Settings' and [IniKeyword] like 'clienturl'"
+
+write-host $query
+
+$Location = "c:\PowerShell\"
+#New-Item -Path $Location -Name "Ezines" -ItemType "directory"
+Invoke-Sqlcmd -ServerInstance Enterprise `
+    -Database CS2010 -UserName sa -Password livelink -Query $query
+
+# sqlcmd -S "Enterprise" -U sa -P livelink -i rename.sql -o rename.log -v url='http://www.yahoo.com'
